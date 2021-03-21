b9bc47af6e80c6d0754e2ec00e8008e881b9f8b7
sajidkhattak1996 <47879467+sajidkhattak1996@users.noreply.github.com>
2021-03-21 21:23:04 +0500

    change

--- plugins/js/my-script.js
+++ plugins/js/my-script.js
@@ -1,120 +1,117 @@
-function pageShow(obj){
-    var btn_id=obj.id;
-    // console.log(btn_id);
-    if(btn_id==="about"){
-      // remove css classes
-      $("#home").removeClass("d-block");
-      $("#login").removeClass("d-block");
-      $("#c_account").removeClass("d-block");
-      $("#contact").removeClass("d-block");
-      $("#cs_account").removeClass("d-block");
-      $("#ct_account").removeClass("d-block"); 
-      // add css classes
-      $("#home").addClass("d-none");
-      $("#menu-tabs").addClass("my-tab");
-      $("#about").addClass("d-block");
-    }
-    else if(btn_id==="c_account"){
-    //   console.log('ok');
-      // remove css classes
-      $("#home").removeClass("d-block");
-      $("#login").removeClass("d-block");
-      $("#about").removeClass("d-block");
-      $("#contact").removeClass("d-block");  
-      $("#cs_account").removeClass("d-block");
-      $("#ct_account").removeClass("d-block");     
-      // add css classes
-      $("#home").addClass("d-none");
-      $("#c_account").addClass("d-block");
-      $("#menu-tabs").addClass("my-tab");
-    }
-    else if(btn_id==="login"){
-       // remove css classes
-       $("#home").removeClass("d-block");
-      $("#c_account").removeClass("d-block");
-      $("#about").removeClass("d-block");
-      $("#contact").removeClass("d-block"); 
-      $("#cs_account").removeClass("d-block");
-      $("#ct_account").removeClass("d-block");      
-      // add css classes
-      $("#home").addClass("d-none");
-      $("#login").addClass("d-block");
-      $("#menu-tabs").addClass("my-tab");
-    }
-    else if(btn_id==="contact"){
-       // remove css classes
-       $("#home").removeClass("d-block");
-      $("#c_account").removeClass("d-block");
-      $("#about").removeClass("d-block");
-      $("#login").removeClass("d-block"); 
-      $("#cs_account").removeClass("d-block");
-      $("#ct_account").removeClass("d-block");     
-      // add css classes
-      $("#home").addClass("d-none");
-      $("#contact").addClass("d-block");
-      $("#menu-tabs").addClass("my-tab");
-    }
-  }
 
-  //function tragger when click on student account 
-  function createStudentAccount(){
+//function for about button are press & his page show 
+function aboutPageShow(){
+  // remove css classes
+  // $("#home").removeClass("d-block");
+  $("#login").removeClass("d-block");
+  $("#c_account").removeClass("d-block");
+  // $("#contact").removeClass("d-block");
+  $("#cs_account").removeClass("d-block");
+  $("#ct_account").removeClass("d-block"); 
+  // add css classes
+  $("#more_details").addClass("d-block");
+  $("#menu-tabs").addClass("my-tab");
+  $("#about").addClass("d-block");
+  $("#carouselExampleControls").addClass("d-none");
+}
+
+//function for create account button are press and his data are load 
+function createAccountPage(){
+    // remove css classes
+    // $("#home").removeClass("d-block");
+    $("#login").removeClass("d-block");
+    $("#about").removeClass("d-block");
+    $("#cs_account").removeClass("d-block");
+    $("#ct_account").removeClass("d-block");     
+    $("#more_details").removeClass("d-block");     
+    // add css classes
+    // $("#home").addClass("d-none");
+    $("#c_account").addClass("d-block");
+    $("#menu-tabs").addClass("my-tab");
+    $("#carouselExampleControls").addClass("d-none");
+    $("#more_details").addClass("d-none");
+}
+
+//function for the login button and login page are displayed 
+function loginPageShow(){
+    // remove css classes
+    $("#c_account").removeClass("d-block");
+    $("#about").removeClass("d-block");
+    $("#cs_account").removeClass("d-block");
+    $("#ct_account").removeClass("d-block");
+    $("#more_details").removeClass("d-block");     
+
+    // add css classes
+    $("#carouselExampleControls").addClass("d-none");
+    $("#more_details").addClass("d-none");
+    $("#login").addClass("d-block");
+    $("#menu-tabs").addClass("my-tab");
+}
+
+
+
+//function tragger when click on student account 
+function createStudentAccount(){
+     // remove css classes
+     $("#home").removeClass("d-block");
+    $("#c_account").removeClass("d-block");
+    $("#about").removeClass("d-block");
+    $("#login").removeClass("d-block"); 
+    $("#ct_account").removeClass("d-block");     
+    // add css classes
+    $("#home").addClass("d-none");
+    $("#c_account").addClass("d-none");
+    $("#menu-tabs").addClass("my-tab");
+    $("#cs_account").addClass("d-block");
+}
+
+  //function tragger when click on Teacher  account 
+  function createTeacherAccount(){
        // remove css classes
        $("#home").removeClass("d-block");
       $("#c_account").removeClass("d-block");
       $("#about").removeClass("d-block");
       $("#login").removeClass("d-block"); 
       $("#ct_account").removeClass("d-block");     
+      $("#cs_account").removeClass("d-block");     
       // add css classes
       $("#home").addClass("d-none");
       $("#c_account").addClass("d-none");
       $("#menu-tabs").addClass("my-tab");
-      $("#cs_account").addClass("d-block");
+      $("#ct_account").addClass("d-block");
   }
 
-    //function tragger when click on Teacher  account 
-    function createTeacherAccount(){
-         // remove css classes
-         $("#home").removeClass("d-block");
-        $("#c_account").removeClass("d-block");
-        $("#about").removeClass("d-block");
-        $("#login").removeClass("d-block"); 
-        $("#ct_account").removeClass("d-block");     
-        $("#cs_account").removeClass("d-block");     
-        // add css classes
-        $("#home").addClass("d-none");
-        $("#c_account").addClass("d-none");
-        $("#menu-tabs").addClass("my-tab");
-        $("#ct_account").addClass("d-block");
-    }
+  // function for click for the login for the account 
+  function Login(){
+       // remove css classes
+       $("#home").removeClass("d-block");
+      $("#c_account").removeClass("d-block");
+      $("#about").removeClass("d-block");
+      $("#ct_account").removeClass("d-block");     
+      $("#cs_account").removeClass("d-block");     
+      // add css classes
+      $("#home").addClass("d-none");
+      $("#c_account").addClass("d-none");
+      $("#menu-tabs").addClass("my-tab");
+      $("#login").addClass("d-block");
+  }
 
-    // function for click for the login for the account 
-    function Login(){
-         // remove css classes
-         $("#home").removeClass("d-block");
-        $("#c_account").removeClass("d-block");
-        $("#about").removeClass("d-block");
-        $("#ct_account").removeClass("d-block");     
-        $("#cs_account").removeClass("d-block");     
-        // add css classes
-        $("#home").addClass("d-none");
-        $("#c_account").addClass("d-none");
-        $("#menu-tabs").addClass("my-tab");
-        $("#login").addClass("d-block");
-    }
+  // function for create account 
+  function CreateAccount(){
+       // remove css classes
+       $("#home").removeClass("d-block");
+      $("#login").removeClass("d-block");
+      $("#about").removeClass("d-block");
+      $("#ct_account").removeClass("d-block");     
+      $("#cs_account").removeClass("d-block");     
+      // add css classes
+      $("#home").addClass("d-none");
+      $("#menu-tabs").addClass("my-tab");
+      $("#c_account").addClass("d-block");
+  }
 
-    // function for create account 
-    function CreateAccount(){
-         // remove css classes
-         $("#home").removeClass("d-block");
-        $("#login").removeClass("d-block");
-        $("#about").removeClass("d-block");
-        $("#ct_account").removeClass("d-block");     
-        $("#cs_account").removeClass("d-block");     
-        // add css classes
-        $("#home").addClass("d-none");
-        $("#menu-tabs").addClass("my-tab");
-        $("#c_account").addClass("d-block");
-    }
+
+
 
     //below tabs button for the different informtation 
 
@@ -142,6 +139,7 @@ function pageShow(obj){
     function ShowTab2(obj){
       $(obj).addClass('active2');
       $("#tab2_info").addClass('d-block');
+
 
       //color reset of tabs
       $("#t1").removeClass("active1");
